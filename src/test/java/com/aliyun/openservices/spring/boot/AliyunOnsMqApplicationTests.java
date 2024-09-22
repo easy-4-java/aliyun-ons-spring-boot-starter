de389ce15548d4821f977601697bd41880dc6532
luffy <luffy@vanillafinance.xyz>
2024-09-22 21:21:58 +0800

    3.3.x.20240823.RELEASE

--- src/test/java/com/aliyun/openservices/spring/boot/AliyunOnsMqApplicationTests.java
+++ src/test/java/com/aliyun/openservices/spring/boot/AliyunOnsMqApplicationTests.java
@@ -1,16 +1,12 @@
 package com.aliyun.openservices.spring.boot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import com.aliyun.openservices.ons.api.Message;
 import com.aliyun.openservices.ons.api.Producer;
 import com.aliyun.openservices.ons.api.order.OrderProducer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class AliyunOnsMqApplicationTests {
 
